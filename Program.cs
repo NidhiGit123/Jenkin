3104b7490b1e1037a28d17a64ecdf083b3ac28db
NidhiGit123 <93639492+NidhiGit123@users.noreply.github.com>
2021-11-11 12:07:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Demo_App_HCSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sayHello("world"));
+        }
+        static string sayHello(string name)
+        {
+            return ("hello " + name );
+        }
+    }
+}
